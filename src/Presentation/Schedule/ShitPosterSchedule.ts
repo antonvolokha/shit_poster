20117991c007ad7af3e994817b72d5bd6e765efe
Anton Volokha <avolokha@portmone.me>
2022-04-27 23:16:17 +0300

    Fixes

--- src/Presentation/Schedule/ShitPosterSchedule.ts
+++ src/Presentation/Schedule/ShitPosterSchedule.ts
@@ -3,26 +3,29 @@ import {coub, rss, telegramShit} from '../../config';
 //import { telegram} from '../../config';
 import {CoubService} from "../../Application/Service/ShitPoster/CoubService";
 import {JoyReactorService} from "../../Application/Service/ShitPoster/JoyReactorService";
-import {TelegramAdapter} from "../../Infrastructure/Adapter/TelegramAdapter";
+//import {TelegramAdapter} from "../../Infrastructure/Adapter/TelegramAdapter";
 import {TelegramChannelService} from "../../Application/Service/ShitPoster/TelegramChannelService";
 
 export const scheduleFunction = new BaseSchedule()
     .setRule('*/1 * * * *')
     .setAction(async () => {
-        const telegramBot = new TelegramAdapter(telegramShit);
+        //const telegramBot = new TelegramAdapter(telegramShit);
         (new JoyReactorService(rss)).getImages().then((images) => {
             images.forEach((image) => {
-                telegramBot.sendPhotoOnce(image);
+                console.log(image);
+                //telegramBot.sendPhotoOnce(image);
             });
         });
         (new CoubService(coub)).getLinks().then((coubs) => {
             coubs.forEach((coub) => {
-                telegramBot.sendMessageOnce(coub);
+                console.log(coub);
+                //telegramBot.sendMessageOnce(coub);
             });
         });
         (new TelegramChannelService(telegramShit)).getImages().then((images) => {
             images.forEach((image) => {
-                telegramBot.sendPhotoOnce(image);
+                console.log(image);
+                //telegramBot.sendPhotoOnce(image);
             });
         });
     }).create();
